8516a0a7f562629f412bff65eca04b2a4aef5a08
kunjfuletra <kunjfuletra123@gmail.com>
2023-07-17 10:41:54 +0530

    first commit

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './Home';
+import Cuisine from './Cuisine';
+import {Route, Routes} from "react-router-dom";
+import Searched from './Searched';
+import Recipe from './Recipe';
+
+function pages() {
+
+  return (
+    
+    <Routes >
+      <Route path="/" element={<Home />}/>
+      <Route path="/cuisine/:type" element={<Cuisine/>}/>
+      <Route path="/searched/:search" element={<Searched/>}/>
+      <Route path ="/recipe/:name" element={<Recipe/>}/>
+    </Routes>
+    
+    
+  )
+}
+
+export default pages
